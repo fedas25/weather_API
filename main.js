befc8f07a239a31dedbb8eba09cd4f22106a4bc3
Fedor <uasily.lol@yandex.ru>
2022-05-11 03:49:03 +0300

    losos

---
+++ main.js
@@ -0,0 +1,25 @@
+let formValue = document.querySelector("input"); 
+let firstName = 'artem';
+const serverUrl = 'https://api.genderize.io';
+let url = `${serverUrl}?name=${firstName}`;
+
+formValue.addEventListener("keydown", function(key) {
+  if (key.code == "Enter") {
+    firstName = formValue.value;
+    url = `${serverUrl}?name=${firstName}`;
+    fetch(url)
+      .then(response => response.json())
+      .then((commits) => {
+      if (commits.gender !== undefined && commits.gender !== null) {
+        alert(firstName + " is " + commits.gender)
+      } else {
+        alert("Чёт не то");
+      }});
+      
+      formValue.value = "";
+  }
+});
+
+
+
+
